72ea6656f26d7650f504c4854cf3ee0445d63468
Subatomi <laderatheo@gmail.com>
2026-03-12 21:56:25 +0800

    Add star particle background effect to home page

---
+++ src/components/StarBackground.tsx
@@ -0,0 +1,11 @@
+import '../styles/starBackground.css'
+
+export default function StarBackground() {
+  return (
+    <div className="star-background-container">
+      <div className="bg-animation">
+        <div id="stars3"></div>
+      </div>
+    </div>
+  )
+}
